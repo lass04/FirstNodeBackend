5c0d8379377e780cc288c6c3c58a4c501c1b73c5
lass04 <yassinelassoued58@gmail.com>
2025-12-13 09:42:54 +0100

    Added Models

---
+++ src/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username:{
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            maxlength: 20,
+            minlength: 4,
+            trim: true
+        },
+
+        password:{
+           type: String,
+           required: true,
+           maxlength: 20,
+           minlength: 8,
+        },
+
+        email:{
+          type:String,
+          required: true,
+          unique: true,
+          maxlength: 30,
+          minlength: 13,
+          trim: true
+        },
+    },
+        {
+            timestamps: true 
+        }
+        
+)
+
+export const User = mongoose.model("User",userSchema);
